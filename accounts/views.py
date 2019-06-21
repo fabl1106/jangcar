1a8009bbdeff6d91919985de817d76665ff60589
byeonguk kim <gang0406gang@gmail.com>
2019-06-21 09:51:43 +0900

    password * set

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
+from urllib.parse import urlparse
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.views.generic.base import View
 
 from accounts import *
@@ -23,6 +26,11 @@ def signup(request):
             user_instance.set_password(signup_form.cleaned_data['password'])
             user_instance.save()
             return render(request, 'accounts/signup_complete.html', {'username':user_instance.username})
+        else:
+            messages.warning(request, '비밀번호 입력이 일치하지 않습니다')
+            referer_url = request.META.get('HTTP_REFERER')
+            path = urlparse(referer_url).path
+            return HttpResponseRedirect(path)
 
     else:
         # todo: from 객체를 만들어서 전달.
